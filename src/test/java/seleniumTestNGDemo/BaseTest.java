d5efe6b634aa2592a19318a97a27f49039f6a0d0
d.stanisavljevic <dragan.stanisavljevic@mapp.com>
2024-07-07 14:41:29 +0200

    Path updated to linux path

--- src/test/java/seleniumTestNGDemo/BaseTest.java
+++ src/test/java/seleniumTestNGDemo/BaseTest.java
@@ -1,19 +1,17 @@
 package seleniumTestNGDemo;
 
 import org.apache.log4j.Logger;
-import org.openqa.selenium.By;
-import org.openqa.selenium.PageLoadStrategy;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.*;
 import utils.CalculateResolution;
 import utils.ConfigUtils;
-import utils.WebDriverUtils;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
 public class BaseTest {
@@ -27,26 +25,40 @@ public class BaseTest {
 
     @Parameters({"brows"})
     @BeforeClass
-    public void startTest(@Optional("chrome") String browser) {
+    public void startTest(@Optional("chrome") String browser) throws MalformedURLException {
         setup(browser);
     }
 
-    public void setup(String browser) {
+    public void setup(String browser) throws MalformedURLException {
         System.setProperty("brows", browser);
         if(System.getProperty("env") == null){
             System.setProperty("env", "qa");
         }
         if (System.getProperty("brows").equalsIgnoreCase("chrome")) {
-            System.setProperty("webdriver.chrome.driver", "src\\test\\resources\\drivers\\chrome\\chromedriver");
+            System.setProperty("webdriver.chrome.driver", "src/test/resources/drivers/chrome/chromedriver");
             ChromeOptions options=new ChromeOptions();
             options.addArguments(CalculateResolution.getResolution());
             options.setPageLoadStrategy(PageLoadStrategy.NONE);
-            driver=new ChromeDriver(options);
+            //driver=new ChromeDriver(options);
+            DesiredCapabilities cap = DesiredCapabilities.chrome();
+            cap.setBrowserName("chrome");
+            cap.setPlatform(Platform.LINUX);
+            //URL gridURL = new URL("http://localhost:4444"); for local grid
+            URL gridURL = new URL("http://selenium-hub:4444");
+            driver = new RemoteWebDriver(gridURL, cap);
             driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         } else if (System.getProperty("brows").equalsIgnoreCase("firefox")) {
             //set uo Firefox driver
-            System.setProperty("webdriver.gecko.driver", "src/test/resources/drivers/firefox/geckodriver");
-            driver =new FirefoxDriver();
+//            driver = new FirefoxDriver();
+//            System.setProperty("webdriver.gecko.driver", "src/test/resources/drivers/firefox/geckodriver");
+//            driver =new FirefoxDriver();
+            DesiredCapabilities cap = DesiredCapabilities.chrome();
+            cap.setBrowserName("firefox");
+            cap.setPlatform(Platform.LINUX);
+            //URL gridURL = new URL("http://localhost:4444"); for local grid
+            URL gridURL = new URL("http://selenium-hub:4444");
+            driver = new RemoteWebDriver(gridURL, cap);
+            driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         } else if (System.getProperty("brows").equalsIgnoreCase("IE")) {
             // setup IE driver
         }
